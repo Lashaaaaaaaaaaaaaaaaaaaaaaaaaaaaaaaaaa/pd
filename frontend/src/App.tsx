63b7395c50945a11fa513f1b99e49f18c45286f3
lasha jijelava <lasha0990@gmail.com>
2025-02-05 02:50:42 +0400

    update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import AppRouter from "./router/AppRouter";
 function App() {
   return (
     <>
-      <h1>Hello there, Dart Vader</h1>
+      <h1>Hello there, Darth Vader</h1>
       <AppRouter />
     </>
   );
